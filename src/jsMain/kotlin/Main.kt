5432406f8205c9b7f5a989b48f2afe370d7bc5af
Nanai Jua <me@n7i.dev>
2023-12-16 12:27:48 +0900

    fix: register event-handler at `addEventListener`

--- src/jsMain/kotlin/Main.kt
+++ src/jsMain/kotlin/Main.kt
@@ -2,8 +2,6 @@ import org.w3c.fetch.Request;
 import org.w3c.fetch.Response;
 import org.w3c.fetch.ResponseInit;
 
-@OptIn(ExperimentalJsExport::class)
-@JsExport
 fun fetch(req: Request): Response {
     val headers: dynamic = object {}
 
@@ -12,4 +10,12 @@ fun fetch(req: Request): Response {
     val body = "hi, kotlin/js?"
 
     return Response(body, ResponseInit(headers = headers))
+}
+
+fun main() {
+    val addEventListener = js("addEventListener");
+
+    addEventListener("fetch") { e ->
+        e.respondWith(fetch(e.request as Request))
+    }
 }
